4425dd391121093e7bc1997aed6ea6274a0148ae
SunJie117 <camark117@163.com>
2018-07-03 08:43:11 +0800

    20180703

---
+++ app/src/main/java/com/camark/androidstudy/com/camark/geoquiz/QuizActivity.java
@@ -0,0 +1,15 @@
+package com.camark.androidstudy.com.camark.geoquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.camark.androidstudy.R;
+
+public class QuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+    }
+}
