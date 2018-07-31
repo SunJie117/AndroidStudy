8080a7268dd7a3a12f383b3ba9fdb916c5f4bef6
SunJie117 <camark117@163.com>
2018-07-31 18:02:40 +0800

    第十章完成

--- app/src/main/java/com/camark/androidstudy/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/camark/androidstudy/criminalintent/CrimeListFragment.java
@@ -92,7 +92,7 @@ public class CrimeListFragment extends Fragment {
 
         @Override
         public void onClick(View v) {
-            Intent intent = CrimeActivity.newIntent(getActivity(), mCrime.getId());
+            Intent intent = CrimePagerActivity.newIntent(getActivity(), mCrime.getId());
             startActivity(intent);
 
         }
@@ -117,7 +117,7 @@ public class CrimeListFragment extends Fragment {
         @Override
         public void onClick(View v) {
 
-            Intent intent = CrimeActivity.newIntent(getActivity(), mCrime.getId());
+            Intent intent = CrimePagerActivity.newIntent(getActivity(), mCrime.getId());
             startActivity(intent);
 
 
