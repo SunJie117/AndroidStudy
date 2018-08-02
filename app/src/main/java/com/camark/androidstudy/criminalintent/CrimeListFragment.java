c67219cf7fd1700f868fdcbd795c69a4b0a28c37
SunJie117 <camark117@163.com>
2018-08-02 18:01:07 +0800

    第十三章

--- app/src/main/java/com/camark/androidstudy/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/camark/androidstudy/criminalintent/CrimeListFragment.java
@@ -7,6 +7,8 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -44,6 +46,12 @@ public class CrimeListFragment extends Fragment {
     public void onResume() {
         super.onResume();
         updateUI();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_crime_list, menu);
     }
 
     private void updateUI() {
